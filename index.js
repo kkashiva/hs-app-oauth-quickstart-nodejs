13941df2a624ca31003ffaf752632bb58ac657d3
Kartikeya Kashiva <kkashiva@gmail.com>
2024-10-13 01:22:09 +0530

    removed portbinging localhost

--- index.js
+++ index.js
@@ -215,5 +215,5 @@ app.get('/error', (req, res) => {
   res.end();
 });
 
-app.listen(PORT, () => console.log(`=== Starting your app on http://localhost:${PORT} ===`));
-opn(`http://localhost:${PORT}`);
+// app.listen(PORT, () => console.log(`=== Starting your app on http://localhost:${PORT} ===`));
+// opn(`http://localhost:${PORT}`);
